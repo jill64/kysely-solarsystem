11460f96cb8b93a2cfddbf44061cabb077f5dad5
jill64 <contact@jill64.dev>
2025-03-10 08:34:37 +0900

    migrate to bun

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,35 +1,27 @@
 import 'dotenv/config'
 import { Kysely } from 'kysely'
 import { env } from 'node:process'
-import { expect, test } from 'vitest'
+import { expect, test } from 'bun:test'
 import { SolarSystemDialect } from '../src'
 import type { Database } from './schema'
 
-test(
-  'E2E',
-  {
-    timeout: 30000
-  },
-  async ({ onTestFailed }) => {
-    const db = new Kysely<Database>({
-      dialect: new SolarSystemDialect({
-        teamName: env.SOLARSYSTEM_TEAM_NAME!,
-        clusterName: env.SOLARSYSTEM_CLUSTER_NAME!,
-        branchName: env.SOLARSYSTEM_BRANCH_NAME!,
-        apiKey: env.SOLARSYSTEM_API_KEY!
-      })
+test('E2E', async () => {
+  const db = new Kysely<Database>({
+    dialect: new SolarSystemDialect({
+      teamName: env.SOLARSYSTEM_TEAM_NAME!,
+      clusterName: env.SOLARSYSTEM_CLUSTER_NAME!,
+      branchName: env.SOLARSYSTEM_BRANCH_NAME!,
+      apiKey: env.SOLARSYSTEM_API_KEY!
     })
+  })
 
-    await db.schema
-      .createTable('user')
-      .addColumn('id', 'integer', (col) => col.primaryKey().autoIncrement())
-      .addColumn('name', 'varchar', (col) => col.notNull())
-      .execute()
+  await db.schema
+    .createTable('user')
+    .addColumn('id', 'integer', (col) => col.primaryKey().autoIncrement())
+    .addColumn('name', 'varchar', (col) => col.notNull())
+    .execute()
 
-    onTestFailed(async () => {
-      await db.schema.dropTable('user').execute()
-    })
-
+  try {
     const create_result = await db
       .insertInto('user')
       .values({
@@ -48,7 +40,10 @@ test(
 
     expect(query_result.id).toBe(1)
     expect(query_result.name).toBe('Alice')
-
+  } catch (e) {
     await db.schema.dropTable('user').execute()
+    throw e
   }
-)
+
+  await db.schema.dropTable('user').execute()
+})
